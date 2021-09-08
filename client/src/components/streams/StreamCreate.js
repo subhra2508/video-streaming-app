c94cb23cbbf26aed92d37b802dc533b2553fe0c8
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-09-09 00:01:09 +0530

    added streams action creatorand reducers,added json server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component{
     // renderInput(formProps){
@@ -25,8 +27,9 @@ class StreamCreate extends React.Component{
         )
     }
 
-    onSubmit(formValues){
-         console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+         
     }
 
     render(){
@@ -54,7 +57,9 @@ const validate = (formValues) => {
 
 
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form:'streamCreate',
     validate:validate
 })(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
