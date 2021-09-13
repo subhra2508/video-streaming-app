a027e1149bb355eeb3bbe1a43c9c11fe16ab265e
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-09-13 15:57:18 +0530

    added edit and delete buttom to the own stream

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,19 +8,25 @@ class StreamList extends React.Component{
     }
     renderAdmin(stream){
       if(stream.userId === this.props.currentuserId){
-          return <div>EDIT/DELETE</div>
+          return (
+              <div className="right floated content">
+                  <button className="ui button primary">Edit</button>
+                  <button className="ui button negative">Delete</button>
+              </div>
+          )
       }  
     }
     renderList(){
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                 {this.renderAdmin(stream)}
                 <i className="large middle aligned icon camera"></i>
                 <div className="content">
                     {stream.title}
                     <div className="description">{stream.description}</div>
                 </div>
-                {this.renderAdmin(stream)}
+               
             </div>
             )
         })
